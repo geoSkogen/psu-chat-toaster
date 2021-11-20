c858a61e47c76481abd1498a93ee7fbf632fff60
Joseph Scoggins <borninthecaul@gmail.com>
2021-11-19 17:19:15 -0800

    adds rules for extra fields - SB grad programs

--- PSUChat.js
+++ PSUChat.js
@@ -5,7 +5,7 @@
 var PSUCHATMODULE = PSUCHATMODULE || {};
 
 PSUCHATMODULE.team = "";
-PSUCHATMODULE.formExpand_remodel = 88;
+PSUCHATMODULE.formExpand_remodel = 80;
 // ADM, FA.
 PSUCHATMODULE.formExpand = 205;
 //Normal Chat Toaster Form
@@ -13,7 +13,7 @@ PSUCHATMODULE.formExpand_noChat = 200;
 //ORG 128
 PSUCHATMODULE.formExpand_AddEmail = 220;
 //Expand Toaster for Email Message
-PSUCHATMODULE.formExpand_AddField = 300;
+PSUCHATMODULE.formExpand_AddField = 316;
 //Expand Toaster for Addtl Fields
 PSUCHATMODULE.headerExpand = 30;
 PSUCHATMODULE.forceheaderopenExpand = 30;
@@ -406,7 +406,7 @@ PSUCHATMODULE.loadTeamSettings = function(theTeam) {
     PSUCHATMODULE.offhoursText = theTeam.offhoursText;
     PSUCHATMODULE.chatWindowHeader = theTeam.chatWindowHeader;
     PSUCHATMODULE.showIDField = theTeam.showIDField;
-    PSUCHATMODULE.showSBGradField = theTeam.showSBGradField;
+    PSUCHATMODULE.showSBGradField = (window.location.pathname.indexOf('sb-grad.html')>-1) ? true : theTeam.showSBGradField;
     PSUCHATMODULE.chatteamactive = theTeam.chatteamactive;
     PSUCHATMODULE.forceheaderopenExpand = theTeam.forceheaderopenExpand;
     PSUCHATMODULE.forceheaderoffExpand = theTeam.forceheaderoffExpand;
@@ -524,27 +524,34 @@ PSUCHATMODULE.loadJS = function() {
 
 PSUCHATMODULE.loadStyle = function() {
     var cStyle = document.createElement("style");
-    var faLink = document.createElement("link")
+    var faLink = document.createElement("link");
+    var gfLink = document.createElement("link");
 
     cStyle.type = "text/css";
+
     faLink.rel = "stylesheet";
     faLink.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css";
 
-    var strStyle = "div.chat_underbar {position: fixed;right:20px;bottom: 0px;height:0px;margin:0px;padding:0px;border:none;width:auto;overflow:hidden;z-index:250;}\n" +
+    gfLink.rel = "stylesheet"
+    gfLink.href = "https://fonts.googleapis.com/css?family=Montserrat";
+
+
+    var strStyle = "div.chat_underbar {position:fixed;right:20px;bottom:0px;height:0px;margin:0px;padding:0px;border:none;width:auto;overflow:hidden;z-index:250;font-family:'Montserrat'}\n" +
     "div.chat_underbar div.chat_offerChat {width:300px;height:auto;margin:0px;padding:0px;border-width:0px;background-color:#ededee;text-align:center;}\n" +
     "div.chat_underbar div.chat_offerNoChat {width:300px;height:200px;margin:0px;padding:0px;background-color:#e86d1f;text-align:center;}\n" +
     "div.chat_header {height:auto;color:#ededee;background-color:black;border:0px;padding:6px 14px;margin:0px;text-align:left;}\n" +
     "div.chat_close {float:right;height:28px;width:21px;padding:0px 0px 0px 0px;color:white;text-align:right;vertical-align:top;}\n" +
-    "div.chat_header a {text-decoration:none;font-size:11px;font-family:\"Lucida Grande\",\"Lucida Sans Unicode\",Verdana,san-serif; color:white;}\n" +
-    "a.chat_headerText {display:block;width:auto;padding:0.33em;font-family:'Montserrat';color:'#ededee';font-size:14px;font-weight:600;}\n" +
+    "div.chat_header a {text-decoration:none;}\n" +
+    "a.chat_headerText {display:block;width:auto;padding:0.33em;font-family:'Montserrat';color:#ededee;font-size:14px;font-weight:600;}\n" +
     "div.chat_header a:hover, div.chat_header a:focus { text-decoration:none; }\n" +
     "div.chat_offerChatForm {height:auto;background-color:#ededee;padding:20px 20px;text-align:left;color:black;}\n" +
     ".chat_controls {width:auto;clear:both;height:auto;}\n" +
     ".field_wrap {display:flex;flex-flow:row wrap;justify-content:center;margin:0 0 0.66em 0;}\n" +
-    ".chat_controls label {font-size:14px;color:#1d252d;display:block;float:none;width:autp;text-align:right;padding:0.33em 0.66em 0 0;margin-bottom:0;font-weight:600;min-width:90px;max-width:95px;}\n" +
+    ".chat_controls label{font-size:14px;color:#1d252d;display:block;float:none;width:auto;text-align:right;padding:0.33em 0.66em 0 0;margin-bottom:0;font-weight:600;min-width:90px;max-width:90px;}\n" +
+    ".chat_controls select {height:2.33em;}\n" +
     ".chat_controls_inner { padding-top:3px;max-width:160px;min-width:160px; }\n" +
     ".chat_controls_invalid {border:solid 2px red;}\n" +
-    ".chat_underbarInput {width:100%;color:#1d252d;font-size:13px;font-weight:600;opacity:0.8;padding:0.15em 0.5em;}\n" +
+    ".chat_underbarInput,.chat_controls select {width:100%;color:#1d252d;font-size:13px;font-weight:600;opacity:0.8;padding:0.15em 0.5em;}\n" +
     ".chat_req_asterisk {color:#1d252d;vertical-align:baselin;font-size:12px;}\n" +
     "input#btnStart2 {color:#ededee;font-weight:600;border-width:0px;background-color:#6a7f10;width:auto;font-size:14px;height:auto;padding:0.5em 1.75em;margin:2em 0;border-radius:0 0 0 0;}\n" +
     "input#btnStart2:hover {text-shadow: 0 0 15px #000000;text-decoration:none;}\n" +
@@ -552,21 +559,22 @@ PSUCHATMODULE.loadStyle = function() {
     ".relshell {position:relative;}" +
     "i.fa-stop-circle {position:absolute;left:-1.33em;top:0.66em;font-size:14px;font-weight:600;color:red;display:none;}";
 
-    var strLongHeaderStyle = "div.chat_underbar {position: fixed;right:20px;bottom: 0px;height:0px;margin:0px;padding:0px;border:none;width:auto;overflow:hidden;z-index:250;}\n" +
+    var strLongHeaderStyle = "div.chat_underbar {position: fixed;right:20px;bottom: 0px;height:0px;margin:0px;padding:0px;border:none;width:auto;overflow:hidden;z-index:250;font-family:'Montserrat'}\n" +
     "div.chat_underbar div.chat_offerChat {width:300px;height:auto;margin:0px;padding:0px;border-width:0px;background-color:#ededee;text-align:center;}\n" +
     "div.chat_underbar div.chat_offerNoChat {width:300px;height:200px;margin:0px;padding:0px;background-color:#e86d1f;text-align:center;}\n" +
     "div.chat_header {height:auto;color:#ededee;background-color:black;border:0px;padding:6px 14px;margin:0px;text-align:left;}\n" +
     "div.chat_close {float:right;height:28px;width:21px;padding:0px 0px 0px 0px;color:white;text-align:right;vertical-align:top;}\n" +
-    "div.chat_header a {text-decoration:none;font-size:11px;font-family:\"Lucida Grande\",\"Lucida Sans Unicode\",Verdana,san-serif; color:white;}\n" +
-    "a.chat_headerText {display:block;width:auto;padding:0.33em;font-family:'Montserrat';color:'#ededee';font-size:14px;font-weight:600;}\n" +
+    "div.chat_header a {text-decoration:none;}\n" +
+    "a.chat_headerText {display:block;width:auto;padding:0.33em;font-family:'Montserrat';color:#ededee;font-size:14px;font-weight:600;}\n" +
     "div.chat_header a:hover, div.chat_header a:focus { text-decoration:none; }\n" +
     "div.chat_offerChatForm {height:auto;background-color:#ededee;padding:20px 20px;text-align:left;color:black;}\n" +
     ".chat_controls {width:auto;clear:both;height:auto;}\n" +
     ".field_wrap {display:flex;flex-flow:row wrap;justify-content:center;margin:0 0 0.66em 0;}\n" +
-    ".chat_controls label {font-size:14px;color:#1d252d;display:block;float:none;width:autp;text-align:right;padding:0.33em 0.66em 0 0;margin-bottom:0;font-weight:600;min-width:90px;max-width:95px;}\n" +
+    ".chat_controls label {font-size:14px;color:#1d252d;display:block;float:none;width:auto;text-align:right;padding:0.33em 0.66em 0 0;margin-bottom:0;font-weight:600;min-width:90px;max-width:90px;}\n" +
+    ".chat_controls select {height:2.33em;}\n" +
     ".chat_controls_inner { padding-top:3px;max-width:160px;min-width:160px; }\n" +
     ".chat_controls_invalid {border:solid 2px red;}\n" +
-    ".chat_underbarInput {width:100%;color:#1d252d;font-size:13px;font-weight:600;opacity:0.8;padding:0.15em 0.5em;}\n" +
+    ".chat_underbarInput,.chat_controls select {width:100%;color:#1d252d;font-size:13px;font-weight:600;opacity:0.8;padding:0.15em 0.5em;}\n" +
     ".chat_req_asterisk {color:#1d252d;vertical-align:baselin;font-size:12px;}\n" +
     "input#btnStart2 {color:#ededee;font-weight:600;border-width:0px;background-color:#6a7f10;width:auto;font-size:14px;height:auto;padding:0.5em 1.75em;margin:2em 0;border-radius:0 0 0 0;}\n" +
     "input#btnStart2:hover {text-shadow: 0 0 15px #000000;text-decoration:none;}\n" +
@@ -589,6 +597,7 @@ PSUCHATMODULE.loadStyle = function() {
         document.body.appendChild(cStyle);
     }
     document.body.appendChild(faLink)
+    document.body.appendChild(gfLink)
 }
 ;
 
